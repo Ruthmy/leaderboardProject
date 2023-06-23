5f9627f667b4fc47473a4c90a43669e2b431d36e
Ruthmy <Ruryemo.exe@gmail.com>
2023-06-23 14:19:11 -0600

    Main functionalities added with the help of the Leaderboard API - Created a new game with its respective ID. - Implemented Refresh button to get all scores stored in the API. Implemented Add button to save a new score in the created game (send data to the API). - Implemented getData and renderScores functions to render elements.

--- src/modules/scores.js
+++ src/modules/scores.js
@@ -1,5 +1,13 @@
 // Get HTML elements
+const addButton = document.querySelector('.add-button');
 const refreshButton = document.querySelector('.refresh');
+const errorElement = document.getElementById('error');
+
+// API elements for Kawaii World game ---------------------------------------------------------
+const urlAPI = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+const idAPI = 'a3tc8eSRVHEFLAZFTCqq';
+
+// Buttons functionality ----------------------------------------------------------------------
 
 // Add event listeners
 
@@ -9,4 +17,83 @@ const refreshScores = () => {
   window.location.reload();
 };
 
+// Add new score to the list and save them into localStorage
+const addScore = () => {
+  const nameInput = document.querySelector('#name').value;
+  const scoreInput = document.querySelector('#score').value;
+
+  if (nameInput && scoreInput && scoreInput !== 0 && scoreInput > 0) {
+    // Save the data into the API
+    const myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+
+    const raw = JSON.stringify({
+      user: nameInput,
+      score: parseInt(scoreInput, 10),
+    });
+
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow',
+    };
+
+    fetch(`${urlAPI}${idAPI}/scores/`, requestOptions)
+      .then((response) => response.text())
+      .catch((error) => error);
+
+    // Reset the form and reload the page
+    setTimeout(() => {
+      document.querySelector('.add-form').reset();
+      window.location.reload();
+    }, 2000);
+  } else if (scoreInput === 0 || scoreInput < 0) {
+    errorElement.innerHTML = '<p>Score must be greater than 0</p>';
+    setTimeout(() => {
+      errorElement.innerHTML = '';
+    }, 3000);
+  } else {
+    errorElement.innerHTML = '<p>Please fill all the fields</p>';
+    setTimeout(() => {
+      errorElement.innerHTML = '';
+    }, 3000);
+  }
+};
+
+addButton.addEventListener('click', addScore);
 refreshButton.addEventListener('click', refreshScores);
+
+// Render results ------------------------------------------------------------------------------
+
+// Render the scored saved in the localStorage
+const renderScores = (data) => {
+  let rating = '';
+  // Sort the scores from highest to lowest
+  data.result.sort((a, b) => b.score - a.score).forEach((score, index) => {
+    rating += `
+    <tr>
+      <td><p>${index + 1}</p></td>
+      <td><p>${score.user}</p></td>
+      <td><p>${score.score}</p></td>
+    </tr>
+    `;
+  });
+
+  // Call the father element and insert the data into
+  const scoresElement = document.querySelector('tbody');
+  scoresElement.innerHTML = rating;
+};
+
+// Add an async function to get the data from the API
+const getData = async (callback) => {
+  try {
+    const response = await fetch(`${urlAPI}${idAPI}/scores/`);
+    const data = await response.json();
+    callback(data);
+  } catch (error) {
+    errorElement.innerHTML = `<p>${error}</p>`;
+  }
+};
+
+getData(renderScores);
